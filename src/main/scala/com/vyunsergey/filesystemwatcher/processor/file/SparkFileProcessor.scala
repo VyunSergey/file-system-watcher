6e13ee787573f2b627e1a0c2c1f52632044ef7f2
Vyun Sergey <vyun.sergey@icloud.com>
2021-02-17 14:45:44 +0300

    add full logic for processing spark files

--- src/main/scala/com/vyunsergey/filesystemwatcher/processor/file/SparkFileProcessor.scala
+++ src/main/scala/com/vyunsergey/filesystemwatcher/processor/file/SparkFileProcessor.scala
@@ -2,6 +2,7 @@ package com.vyunsergey.filesystemwatcher.processor.file
 
 import cats.Monad
 import cats.effect.Resource
+import cats.syntax.traverse._
 import com.vyunsergey.filesystemwatcher.common.context.Context
 import tofu.logging._
 import tofu.syntax.logging._
@@ -18,13 +19,32 @@ class SparkFileProcessor[F[_]: Monad: Logging: FileProcessor](context: Context) 
       (config, fileProcessor) <- info"Processing Spark .CSV files in Paths: (meta = '${metaFilePath.toAbsolutePath.toString}', data = '${dataFilePath.toAbsolutePath.toString}')" as {
         (context.config, implicitly[FileProcessor[F]])
       }
-      _ <- println(
-        s"""
-           |*** IN SparkFileProcessor.processFile ***
-           |meta Path: '${metaFilePath.toAbsolutePath.toString}'
-           |data Path: '${dataFilePath.toAbsolutePath.toString}'
-           |
-           |""".stripMargin).pure[F]
+      _ <- info"Finding Output Meta Directory from Path: '${metaFilePath.toAbsolutePath.toString}'"
+      outputMetaPath <- fileProcessor.findParent(metaFilePath, _.getFileName.toString, Some("meta".r)).map(_.getOrElse(metaFilePath.getParent))
+      _ <- info"Finding Output Data Directory from Path: '${dataFilePath.toAbsolutePath.toString}'"
+      outputDataPath <- fileProcessor.findParent(dataFilePath, _.getFileName.toString, Some("data".r)).map(_.getOrElse(dataFilePath.getParent))
+      tempPath = outputMetaPath.getParent.resolve(s"temp")
+      _ <- info"Copping Meta DataFiles from Path: ${outputMetaPath.toAbsolutePath.toString} to Path: '${tempPath.toAbsolutePath.toString}'"
+      _ <- fileProcessor.copyFiles(outputMetaPath, tempPath)
+      _ <- info"Copping Data DataFiles from Path: ${outputDataPath.toAbsolutePath.toString} to Path: '${tempPath.toAbsolutePath.toString}'"
+      _ <- fileProcessor.copyFiles(outputDataPath, tempPath, clearTarget = false)
+      subPaths <- fileProcessor.getSubPaths(tempPath)
+      notDataFiles = subPaths.filter(!_.getFileName.toString.matches(config.csvMask))
+      _ <- info"Deleting Not DataFiles: ${notDataFiles.map(_.getFileName.toString).mkString("[", ",", "]")} from Path: '${tempPath.toAbsolutePath.toString}'"
+      _ <- notDataFiles.traverse(fileProcessor.deleteFiles)
+      _ <- info"Renaming DataFiles with transfer mask in Path: '${tempPath.toAbsolutePath.toString}'"
+      dataPaths = subPaths.filter(_.getFileName.toString.matches(config.csvMask))
+      modifiedTimePath <- dataPaths.traverse(fileProcessor.pathLastModifiedTime)
+      sortedDataPaths = dataPaths.zip(modifiedTimePath).sortBy(_._2.toMillis).map(_._1)
+      _ <- sortedDataPaths.zipWithIndex.traverse { case (path, ind) => fileProcessor.renameFile(path, config.transferFileMask.replaceFirst("<NUM>", ind.toString)) }
+      _ <- info"Finding Output Directory from Path: '${tempPath.toAbsolutePath.toString}'"
+      outputPath <- fileProcessor.findParent(tempPath, _.getFileName.toString, Some("out".r)).map(_.getOrElse(tempPath.getParent))
+      transferPath = outputPath.getParent.resolve(s"transfer")
+      _ <- info"Zipping in archive DataFiles in Path: ${tempPath.toAbsolutePath.toString} to Path: '${transferPath.toAbsolutePath.toString}'"
+      tempSubPaths <- fileProcessor.getSubPaths(tempPath)
+      isFileSubPaths <- tempSubPaths.traverse(fileProcessor.isFile)
+      dataFiles = tempSubPaths.zip(isFileSubPaths).filter(_._2).map(_._1)
+      _ <- fileProcessor.zipFiles(dataFiles, transferPath.resolve(config.transferArcMask))
     } yield ()
   }
 }
