587f946b68875e543c9bbb3e048d119bebdfe1e7
Vyun Sergey <vyun.sergey@icloud.com>
2021-02-20 17:19:50 +0300

    add logic for empty files
    (cherry picked from commit 60bf8aedca12888d43230efb60a630e6a0c175f9)

--- src/main/scala/com/vyunsergey/filesystemwatcher/processor/file/SparkFileProcessor.scala
+++ src/main/scala/com/vyunsergey/filesystemwatcher/processor/file/SparkFileProcessor.scala
@@ -29,22 +29,37 @@ class SparkFileProcessor[F[_]: Monad: Logging: FileProcessor](context: Context) 
       _ <- info"Copping Data DataFiles from Path: ${outputDataPath.toAbsolutePath.toString} to Path: '${tempPath.toAbsolutePath.toString}'"
       _ <- fileProcessor.copyFiles(outputDataPath, tempPath, clearTarget = false)
       subPaths <- fileProcessor.getSubPaths(tempPath)
-      notDataFiles = subPaths.filter(!_.getFileName.toString.matches(config.csvMask))
+      notDataFiles <- subPaths.traverse { path =>
+        (path.pure[F],
+          path.getFileName.toString.matches(config.csvMask).pure[F],
+          fileProcessor.isExist(path),
+          fileProcessor.isFile(path),
+          fileProcessor.pathSize(path).map(_ > 0)
+        ).tupled
+      }.map(_.filter(a => !(a._2 && a._3 && a._4 && a._5)).map(_._1))
       _ <- info"Deleting Not DataFiles: ${notDataFiles.map(_.getFileName.toString).mkString("[", ",", "]")} from Path: '${tempPath.toAbsolutePath.toString}'"
       _ <- notDataFiles.traverse(fileProcessor.deleteFiles)
       _ <- info"Renaming DataFiles with transfer mask in Path: '${tempPath.toAbsolutePath.toString}'"
-      dataPaths = subPaths.filter(_.getFileName.toString.matches(config.csvMask))
-      modifiedTimePath <- dataPaths.traverse(fileProcessor.pathLastModifiedTime)
-      sortedDataPaths = dataPaths.zip(modifiedTimePath).sortBy(_._2.toMillis).map(_._1)
-      _ <- sortedDataPaths.zipWithIndex.traverse { case (path, ind) => fileProcessor.renameFile(path, config.transferFileMask.replaceFirst("<NUM>", ind.toString)) }
+      dataFiles = subPaths.filter(path => !notDataFiles.contains(path))
+      sortedDataFiles <- dataFiles.traverse { path =>
+        (path.pure[F],
+          fileProcessor.pathLastModifiedTime(path)
+        ).tupled
+      }.map(_.sortBy(_._2.toMillis).map(_._1))
+      _ <- sortedDataFiles.zipWithIndex.traverse { case (path, ind) =>
+        fileProcessor.renameFile(path, config.transferFileMask.replaceFirst("<NUM>", ind.toString))
+      }
       _ <- info"Finding Output Directory from Path: '${tempPath.toAbsolutePath.toString}'"
       outputPath <- fileProcessor.findParent(tempPath, _.getFileName.toString, Some("out".r)).map(_.getOrElse(tempPath.getParent))
       transferPath = outputPath.getParent.resolve(s"transfer")
       _ <- info"Zipping in archive DataFiles in Path: ${tempPath.toAbsolutePath.toString} to Path: '${transferPath.toAbsolutePath.toString}'"
-      tempSubPaths <- fileProcessor.getSubPaths(tempPath)
-      isFileSubPaths <- tempSubPaths.traverse(fileProcessor.isFile)
-      dataFiles = tempSubPaths.zip(isFileSubPaths).filter(_._2).map(_._1)
-      _ <- fileProcessor.zipFiles(dataFiles, transferPath.resolve(config.transferArchive))
+      resSubPaths <- fileProcessor.getSubPaths(tempPath)
+      resDataFiles <- resSubPaths.traverse { path =>
+        (path.pure[F],
+          fileProcessor.isFile(path)
+        ).tupled
+      }.map(_.filter(_._2).map(_._1))
+      _ <- fileProcessor.zipFiles(resDataFiles, transferPath.resolve(config.transferArchive))
       _ <- info"Creating Marker Transfer File in Path: '${transferPath.toAbsolutePath.toString}'"
       _ <- fileProcessor.deleteFile(transferPath.resolve(config.transferMarker))
       _ <- fileProcessor.createFile(transferPath.resolve(config.transferMarker))
